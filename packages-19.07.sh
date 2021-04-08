422f4ab8ce08a8b41d2d0e74d34bf17786181faa
WoChen5770 <2012966004@smail.xtu.edu.cn>
2021-04-08 10:35:36 +0800

    Update packages-19.07.sh

--- packages-19.07.sh
+++ packages-19.07.sh
@@ -1,73 +1,100 @@
 #!/bin/bash
 svn co https://github.com/WoChen5770/AdGuardHome/trunk/adguardhome
+svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/ctcgfw/gost
 svn co https://github.com/Lienol/openwrt/branches/19.07/package/diy/luci-app-adguardhome
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial
-git clone https://github.com/rosywrt/luci-theme-rosy
-svn co https://github.com/garypang13/luci-theme-edge/branches/18.06 ./luci-theme-edge
-svn co https://github.com/garypang13/luci-app-dnsfilter/trunk ./luci-app-dnsfilter
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-eqos
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus
-svn co https://github.com/lwz322/luci-app-k3screenctrl/trunk ./luci-app-k3screenctrl
-svn co https://github.com/lwz322/k3screenctrl/trunk ./k3screenctrl
-svn co https://github.com/lwz322/k3screenctrl_build/trunk ./k3screenctrl_build
-svn co https://github.com/siropboy/sirpdboy-package/trunk/uci-app-koolproxyR
-svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-theme-opentomcat
-git clone https://github.com/koshev-msk/luci-app-modeminfo
-svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-netdata
-svn co https://github.com/siropboy/sirpdboy-package/trunk/netdata
-
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/pdnsd-alt
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dns2socks
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/simple-obfs
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/kcptun
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ipt2socks
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/trojan
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray-plugin
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/shadowsocksr-libev
-
-svn co https://github.com/jerrykuku/luci-theme-argon/branches/18.06 ./luci-theme-argon
-svn co https://github.com/jerrykuku/luci-app-argon-config/trunk ./luci-app-argon-config
-svn co https://github.com/esirplayground/luci-app-poweroff/trunk ./luci-app-poweroff
+svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/ntlf9t/luci-app-advancedsetting
 svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-aliddns
-svn co https://github.com/firker/diy-ziyong/trunk/cpulimit-ng
-svn co https://github.com/firker/diy-ziyong/trunk/cpulimit
-svn co https://github.com/firker/diy-ziyong/trunk/luci-app-cpulimit
-svn co https://github.com/siropboy/mypackages/trunk/luci-app-advanced
-svn co https://github.com/sirpdboy/luci-theme-opentopd/trunk ./luci-theme-opentopd
-svn co https://github.com/immortalwrt/openwrt-gowebdav/trunk ./
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-gost
-svn co https://github.com/kenzok8/openwrt-packages/trunk/gost
-svn co https://github.com/iamaluckyguy/luci-app-smartinfo/trunk ./luci-app-smartinfo
-svn co https://github.com/tuanqing/install-program/trunk ./install-program
-svn co https://github.com/tuanqing/openwrt-mentohust/trunk ./openwrt-mentohust
-svn co https://github.com/tuanqing/luci-app-mentohust/trunk ./luci-app-mentohust
+svn co https://github.com/frainzy1477/luci-app-clash/trunk ./luci-app-clash
+rm -rf luci-app-openclash
+rm -rf OpenClash
+git clone https://github.com/vernesong/OpenClash.git
+mv -f OpenClash/luci-app-openclash .
+rm -rf OpenClash
+svn co https://github.com/garypang13/luci-app-eqos/trunk ./luci-app-eqos
+svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/ctcgfw/luci-app-gost
+svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk ./luci-app-jd-dailybonus
+svn co https://github.com/jerrykuku/luci-theme-argon/trunk ./luci-theme-argon
+svn co https://github.com/jerrykuku/luci-app-vssr/trunk ./luci-app-vssr
+svn co https://github.com/tty228/luci-app-serverchan/trunk ./luci-app-serverchan
+svn co https://github.com/fw876/helloworld/trunk ./
+rm -rf .svn
+#
+svn co https://github.com/xiaorouji/openwrt-passwall/trunk ./luci-app-passwall
 svn co https://github.com/msylgj/luci-app-tencentddns/trunk ./luci-app-tencentddns
-svn co https://github.com/jerrykuku/luci-app-ttnode/trunk ./luci-app-ttnode
-svn co https://github.com/zzsj0928/luci-app-serverchand/trunk ./luci-app-serverchand
-svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-socat
-svn co https://github.com/pymumu/openwrt-smartdns/trunk/smartdns
-svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ntlf9t/luci-app-oscam
-svn co https://github.com/immortalwrt/immortalwrt/trunk/package/ntlf9t/oscam
-svn co https://github.com/garypang13/openwrt-packages/trunk/lua-maxminddb
-svn co https://github.com/garypang13/smartdns-le/trunk ./smartdns-le
-svn co https://github.com/immortalwrt/immortalwrt/trunk/package/lean/v2ray
-svn co https://github.com/CCnut/feed-netkeeper/branches/LUCI-LUA-UCITRACK ./luci-app-netkeeper-interception
-git clone https://github.com/linkease/ddnsto-openwrt && mv ddnsto-openwrt/* ./
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/luci-app-autotimeset
+#
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
+svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-argon_new
+svn co https://github.com/kenzok8/luci-theme-ifit/trunk/luci-theme-ifit
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial
+svn co https://github.com/garypang13/luci-theme-edge/trunk ./luci-theme-edge
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-opentomato
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-opentomcat
+svn co https://github.com/jerrykuku/node-request/trunk ./node-request
+svn co https://github.com/jerrykuku/lua-maxminddb/trunk ./lua-maxminddb
+svn co https://github.com/rosywrt/luci-theme-rosy/trunk/luci-theme-rosy
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/UnblockNeteaseMusic
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/UnblockNeteaseMusicGo
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/adbyby
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/baidupcs-web
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-baidupcs-web
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-adbyby-plus
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-airplay2
+svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom
+svn co https://github.com/cnzd/luci-app-koolproxyR/trunk ./luci-app-koolproxyR
+svn co https://github.com/MiRouter/luci-app-vssr-plus/trunk ./luci-app-vssr-plus
+svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/ctcgfw/GoQuiet
+svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/ctcgfw/ChinaDNS
+svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
+svn co https://github.com/lisaac/luci-lib-docker/trunk/collections/luci-lib-docker
+svn co https://github.com/lisaac/luci-app-diskman/trunk ./luci-app-diskman
+rm -rf parted
+mkdir parted
+wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O parted/Makefile
+svn co https://github.com/destan19/OpenAppFilter/trunk ./
+rm -rf .svn
+svn co https://github.com/4IceG/luci-app-3ginfo/trunk/luci-app-3ginfo
+rm -rf luci-app-wrtbwmon
+rm -rf wrtbwmonluci
+git clone https://github.com/brvphoenix/luci-app-wrtbwmon.git wrtbwmonluci
+mv -f wrtbwmonluci/luci-app-wrtbwmon .
+rm -rf wrtbwmonluci
+svn co https://github.com/riverscn/openwrt-iptvhelper/trunk/iptvhelper
+svn co https://github.com/riverscn/openwrt-iptvhelper/trunk/luci-app-iptvhelper
+svn co https://github.com/brvphoenix/wrtbwmon/trunk/wrtbwmon
+svn co https://github.com/KFERMercer/luci-app-tcpdump/trunk ./luci-app-tcpdump
+svn co https://github.com/tty228/luci-app-nodogsplash/trunk ./luci-app-nodogsplash
+svn co https://github.com/koshev-msk/luci-app-atinout/trunk ./luci-app-atinout
+cp -r ./luci-app-atinout/atinout ./
+svn co https://github.com/muink/luci-app-pcap-dnsproxy/branches/dev ./luci-app-pcap-dnsproxy
+svn co https://github.com/koshev-msk/3proxy-openwrt/trunk ./3proxy
+svn co https://github.com/pexcn/openwrt-chinadns-ng/branches/luci ./luci-app-chinadns-ng
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-usb-printer
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ramfree
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ttyd
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-unblockmusic
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-vlmcsd
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/vlmcsd
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-vsftpd
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-xlnetacc
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-frpc
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/frp
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-frps
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_aliyun
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_dnspod
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-cifs-mount
+git clone https://github.com/koshev-msk/luci-app-modeminfo
+svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/ntlf9t/openwrt-Pcap_DNSProxy
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/tcppingull
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dnsforwarder
+svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/ntlf9t/openwrt-udp2raw
+svn co https://github.com/obsy/packages/trunk/3ginfo
+git clone https://github.com/jerrykuku/luci-app-argon-config
+svn co https://github.com/BCYDTZ/luci-app-UUGameAcc/trunk ./luci-app-UUGameAcc
 
 rm -rf ./*/.git
-rm -rf ./*/.svn
-rm -rf ./*/*/.svn
-rm -rf ./*/*/.git
-
-rm -rf ./ReadMe.md
-rm -rf ./*/ReadMe.md
-rm -rf ./*/README.md
-rm -rf ./*/LICENSE
-rm -rf .svn
-rm -rf ./*/readme.txt
+rm -rf ./*/.svn 
 rm -f .gitattributes .gitignore
 exit 0
